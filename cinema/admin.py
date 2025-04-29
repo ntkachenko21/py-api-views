2cc3579f29f45a31269d2c88cd1ca051e57fe46e
Nikita Tkachenko <7heswatgg@gmail.com>
2025-04-29 02:01:02 +0200

    create models, serializers and views

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from cinema.models import Movie
+from cinema.models import (
+    Movie,
+    Actor,
+    Genre,
+    CinemaHall,
+)
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(CinemaHall)
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,16 +1,40 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Actor, Genre, CinemaHall
 
 
-class MovieSerializer(serializers.Serializer):
+class MovieSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
     duration = serializers.IntegerField()
+    actors = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Actor.objects.all(),
+    )
+    genres = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Genre.objects.all(),
+    )
+
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        actors = validated_data.pop("actors")
+        genres = validated_data.pop("genres")
+        movie = Movie.objects.create(**validated_data)
+
+        if isinstance(actors, list):
+            movie.actors.set(actors)
+
+        if isinstance(genres, list):
+            movie.genres.set(genres)
+
+        return movie
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
@@ -19,6 +43,30 @@ class MovieSerializer(serializers.Serializer):
         )
         instance.duration = validated_data.get("duration", instance.duration)
 
+        if isinstance(actors := validated_data.get("actors"), list):
+            instance.actors.set(actors)
+
+        if isinstance(genres := validated_data.get("genres"), list):
+            instance.genres.set(genres)
+
         instance.save()
 
         return instance
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
